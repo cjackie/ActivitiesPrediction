bdf9b10306203fb947fe0e26fef7028c27729109
cjackie <chaojie566@gmail.com>
2016-12-28 21:37:17 -0500

    create service

--- ActivityRecognition/wear/src/main/java/stonybrook/ese/seniordesign/activityrecognition/MainActivity.java
+++ ActivityRecognition/wear/src/main/java/stonybrook/ese/seniordesign/activityrecognition/MainActivity.java
@@ -1,12 +1,16 @@
 package stonybrook.ese.seniordesign.activityrecognition;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.wearable.view.WatchViewStub;
@@ -22,30 +26,17 @@ import com.google.android.gms.wearable.Wearable;
 
 import stonybrook.ese.seniordesign.activityrecognition.sensordata.AccelerometerDataItem;
 
-public class MainActivity extends Activity implements SensorEventListener,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+public class MainActivity extends Activity implements ServiceConnection {
 
     final static String TAG = "ACTIVITY_RECOGNITION_W";
 
-    private GoogleApiClient mGoogleApiClient;
-    private AccelerometerDataItem mAccelData;
     private TextView mTextView;
-    private SensorManager mSensorManager;
-    private Sensor mAccelerameter;
-    private TextView mTextX, mTextY, mTextZ;
+    private Boolean mSensorDataCollectingServiceConnected;
+    private SensorDataCollectingService mSensorDataCollectingService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
-                .build();
-        mGoogleApiClient.registerConnectionCallbacks(this);
-        mGoogleApiClient.registerConnectionFailedListener(this);
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerameter = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mAccelData = null;
 
         setContentView(R.layout.activity_main);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
@@ -53,72 +44,34 @@ public class MainActivity extends Activity implements SensorEventListener,
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
-                mTextX = (TextView)stub.findViewById(R.id.textX);
-                mTextY = (TextView)stub.findViewById(R.id.textY);
-                mTextZ = (TextView)stub.findViewById(R.id.textZ);
+                mTextView.setText("Service for collecting sensor data has started.");
             }
         });
+        startService(new Intent(this, SensorDataCollectingService.class));
+
+        mSensorDataCollectingServiceConnected = false;
+        bindService(new Intent(this, SensorDataCollectingService.class), this, BIND_ABOVE_CLIENT);
 
     }
 
     @Override
     protected void onResume() {
-        Log.d(TAG, "on resume");
         super.onResume();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onPause() {
-        Log.d(TAG, "execute on pause ");
-        super.onPause();
-        mSensorManager.unregisterListener(this);
-    }
-
-
-    public void onSensorChanged(SensorEvent event) {
-
-        switch (event.sensor.getType()) {
-            case Sensor.TYPE_ACCELEROMETER:
-                // temp
-                mTextX.setText(" " + event.values[0]);
-                mTextY.setText(" " + event.values[1]);
-                mTextZ.setText(" " + event.values[2]);
-                // end of temp
-
-                AccelerometerDataItem item = new AccelerometerDataItem(event.values[0],
-                        event.values[1], event.values[2], event.timestamp);
-                PutDataRequest request = PutDataRequest.create(AccelerometerDataItem.PATH);
-                request.setData(item.getData());
-                Wearable.DataApi.putDataItem(mGoogleApiClient, request);
-                break;
-            default:
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        return;
     }
 
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        Log.d(TAG, "google connection succeeded.");
-        mSensorManager.registerListener(this, mAccelerameter, mSensorManager.SENSOR_DELAY_NORMAL);
+    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+        mSensorDataCollectingServiceConnected = true;
+        mSensorDataCollectingService =
+                ((SensorDataCollectingService.SensorDataCollectingServiceLocalBinder)iBinder).getService();
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
-        Log.w(TAG, "google api connection suspended.");
+    public void onServiceDisconnected(ComponentName componentName) {
+        mSensorDataCollectingServiceConnected = false;
+        mSensorDataCollectingService = null;
     }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.e(TAG, "connection to google api failed.");
-        throw new RuntimeException("google api connection fail...");
-    }
-
 }
 
 
--- ActivityRecognition/wear/src/main/java/stonybrook/ese/seniordesign/activityrecognition/SensorDataCollectingService.java
+++ ActivityRecognition/wear/src/main/java/stonybrook/ese/seniordesign/activityrecognition/SensorDataCollectingService.java
@@ -1,47 +1,114 @@
 package stonybrook.ese.seniordesign.activityrecognition;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
-import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.WearableListenerService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
 
-public class SensorDataCollectingService extends WearableListenerService {
+import stonybrook.ese.seniordesign.activityrecognition.sensordata.AccelerometerDataItem;
 
+public class SensorDataCollectingService extends Service implements SensorEventListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    public SensorDataCollectingService() {
-    }
+    final static String TAG = "ACTIVITY_RECOGNITION_S";
+
+    private GoogleApiClient mGoogleApiClient;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerameter;
+    private Boolean mStarted;
+
+    public SensorDataCollectingService() { }
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+        mGoogleApiClient.registerConnectionCallbacks(this);
+        mGoogleApiClient.registerConnectionFailedListener(this);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerameter = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mStarted = false;
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
+    public void onConnected(@Nullable Bundle bundle) {
+        mSensorManager.registerListener(this, mAccelerameter, mSensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
-    public void onDataChanged(DataEventBuffer dataEventBuffer) {
-        super.onDataChanged(dataEventBuffer);
+    public void onConnectionSuspended(int i) {
+        Log.w(TAG, "google api connection suspended.");
     }
 
     @Override
-    public void onMessageReceived(MessageEvent messageEvent) {
-        super.onMessageReceived(messageEvent);
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG, "connection to google api failed.");
+        throw new RuntimeException("google api connection fail...");
     }
 
     @Override
-    public void onPeerConnected(Node node) {
-        super.onPeerConnected(node);
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                AccelerometerDataItem item = new AccelerometerDataItem(event.values[0],
+                        event.values[1], event.values[2], event.timestamp);
+                PutDataRequest request = PutDataRequest.create(AccelerometerDataItem.PATH);
+                request.setData(item.getData());
+                Wearable.DataApi.putDataItem(mGoogleApiClient, request);
+                break;
+            default:
+        }
     }
 
     @Override
-    public void onPeerDisconnected(Node node) {
-        super.onPeerDisconnected(node);
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
+
+
+    // Because this service runs on the same process, other threads can access it:
+    //  ((SensorDataCollectingServiceLocalBinder) mBinder).getService() to get the
+    //  reference to this object.
+    public class SensorDataCollectingServiceLocalBinder extends Binder {
+        SensorDataCollectingService getService() {
+            return SensorDataCollectingService.this;
+        }
+    }
+    private IBinder mBinder = new SensorDataCollectingServiceLocalBinder();
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        synchronized (mStarted) {
+            if (mStarted == true) {
+                return START_STICKY;
+            }
+        }
+
+        mGoogleApiClient.connect();
+        mStarted = true;
+
+        return START_STICKY;
+    }
+
 }
