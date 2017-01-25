e0b3922ced1594ec0a46711ea08d1411dfe552d5
cjackie <chaojie566@gmail.com>
2017-01-25 10:05:52 -0500

    clean up

--- Activity_Recognition_Gyroscope/mobile/src/main/java/com/example/luyu/activity_recognition_gyroscope/sensordata/SensorDataItem.java
+++
@@ -1,9 +0,0 @@
-package com.example.luyu.activity_recognition_gyroscope.sensordata;
-
-/**
- * Created by luyu on 1/1/2017.
- */
-public interface SensorDataItem {
-    byte[] getData();
-    SensorDataItem setData(byte[] data);
-}
